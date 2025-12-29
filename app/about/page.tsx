a97c14e12e59618e32ea33b44ec5d7741c457a31
sazal <sazals48@gmail.com>
2025-12-29 10:38:41 +0545

    Remove AboutTeam component from About page, commenting it out for potential future use.

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -3,7 +3,6 @@ import { AboutStats } from "@/components/about/AboutStats";
 import { AboutShowcase } from "@/components/about/AboutShowcase";
 import { AboutMissionValues } from "@/components/about/AboutMissionValues";
 import { AboutGallery } from "@/components/about/AboutGallery";
-import { AboutTeam } from "@/components/about/AboutTeam";
 
 export default function About() {
   return (
@@ -13,7 +12,7 @@ export default function About() {
       <AboutShowcase />
       <AboutMissionValues />
       <AboutGallery />
-      <AboutTeam />
+      {/* <AboutTeam /> */}
     </>
   );
 }
